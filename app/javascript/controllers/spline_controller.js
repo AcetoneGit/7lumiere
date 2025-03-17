ca7971e8cf03a18da96029eb2d21d6fe299b57d7
Acetone <antoine.c@etik.com>
2025-03-17 13:49:36 +0100

    fix

--- app/javascript/controllers/spline_controller.js
+++ app/javascript/controllers/spline_controller.js
@@ -1,10 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
-import { Application } from '@splinetool/runtime';
+import { Application } from '@splinetool/runtime'
 
+// Connects to data-controller="spline"
 export default class extends Controller {
   connect() {
-    const canvas = this.element
-    const spline = new Application(canvas)
-    spline.load('assets/scene.splinecode')
+    document.addEventListener("turbo:load", () => {
+      const canvas = document.getElementById('canvas3d');
+      if (canvas) {
+        const app = new Application(canvas);
+        app.load('https://prod.spline.design/d4inuz5JIo3UYlZs/scene.splinecode')
+          .then(() => {
+
+            const testCamera = app.findObjectByName("Test");
+            if (testCamera) {
+              console.log("J'ai trouvé la caméra Test");
+              app.setCamera(testCamera);
+            }
+          });
+      }
+    });
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@splinetool/runtime", to: "https://unpkg.com/@splinetool/runtime@0.9.490/build/runtime.js"
+pin "@splinetool/runtime", to: "@splinetool--runtime.js" # @1.9.76
